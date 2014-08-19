ec650f0c8284244bc601c751e76bb1d23cc4cf48
Hugo Torres <hugo.torres1993@gmail.com>
2014-08-19 13:39:04 -0400

    Added a comment explaining require path in skypal-server.rb

--- skypal-server/app/skypal-server.rb
+++ skypal-server/app/skypal-server.rb
@@ -1,11 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
 
+# any files required from the app itself need to be required relative to
+# config.ru because when rackup is run it runs that file and basically all that
+# file does is load SkypalServer and call run
 require "./app/hi"
 
 class SkypalServer < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :database_file, "../config/database.yml"
 
+  # add the classes representing the routes here
   use Hi
 end
